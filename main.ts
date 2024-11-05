a402c79826cbf76e3312318438a0bfa2f570c09f
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-11-05 17:52:59 -0500

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Arsen Koshparenko
+ * Created on: Nov 2024
+ * This program make square out of sprites
 */
 
-basic.showString('Hello, World!')
+let sprite: game.LedSprite = null
+let sideCounter = 0
+let stepCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// Button A is pressed
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    basic.pause(500)
+
+    sideCounter = 0
+    while (sideCounter <= 5) {
+        stepCounter = 0
+        while (stepCounter <= 5) {
+            sprite.move(1)
+            basic.pause(500)
+            stepCounter += 1
+        }
+        // turn 90 degrees
+        sprite.turn(Direction.Right, 90)
+        sideCounter += 1
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
